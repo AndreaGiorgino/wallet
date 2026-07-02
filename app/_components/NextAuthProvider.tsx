a1b441a2add6e83efc85cc1f9c89261e1539093d
giorgi.page <andreagiorgino1@gmail.com>
2026-07-02 19:50:27 +0200

    auth provider implemented

---
+++ app/_components/NextAuthProvider.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react"
+import { ReactNode } from "react"
+
+export default function NextAuthProvider({
+    children,
+}: Readonly<{
+    children: ReactNode,
+}>) {
+    return (
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    );
+}
