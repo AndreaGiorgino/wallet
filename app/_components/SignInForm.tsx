096184901653855e69b2926e4473331c390ca44f
giorgi.page <andreagiorgino1@gmail.com>
2026-07-02 19:52:08 +0200

    signin api call implemented

--- app/_components/SignInForm.tsx
+++ app/_components/SignInForm.tsx
@@ -1,12 +1,47 @@
+import { SubmitEvent, useState } from "react";
 import Button from "./Button";
 import TextInput from "./TextInput";
 
 export default function SignInForm() {
+    const [error, setError] = useState<string | null>(null);
+
+    const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError(null);
+
+        const form = e.target;
+        const data = new FormData(form);
+
+        if (data.get("password") !== data.get("confirm_password")) {
+            setError("Passwords does not match.");
+            return null;
+        }
+
+        const res = await fetch("http://localhost:8080/login", {
+            method: 'POST',
+            body: JSON.stringify({
+                first_name: data.get("first_name"),
+                last_name: data.get("last_name"),
+                email: data.get("email"),
+                password: data.get("password"),
+            }),
+            headers: { "Content-Type": "application/json" }
+        });
+
+        const user = await res.json()
+        if (res.ok && user) {
+            return user;
+        } else {
+            setError("Somethin went wrong.");
+            return null;
+        }
+    };
+
     return (
         <div className="flex flex-col w-[30em] px-[2.5em]">
             <h2 className="text-3xl mb-6">Sign In</h2>
             <hr className="mb-6" />
-            <form className="bg-neutral-900 rounded-lg p-6 w-[25em]">
+            <form className="bg-neutral-900 rounded-lg p-6 w-[25em]" onSubmit={handleSubmit}>
                 <div className="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
                         <TextInput id="first_name" placeholder="Jhon" label="First name" required></TextInput>
@@ -29,6 +64,11 @@ export default function SignInForm() {
                 <div className="flex justify-end text-sm">
                     <Button type="submit" label="Submit"></Button>
                 </div>
+                {error && (
+                    <div className="bg-red-900/40 px-6 py-3 mt-6 rounded-lg">
+                        {error}
+                    </div>
+                )}
             </form>
         </div>
     );
