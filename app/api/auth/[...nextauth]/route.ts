ac782b29a838cf4a9aac0f29477e1b4ee67a5d3c
giorgi.page <andreagiorgino1@gmail.com>
2026-07-02 19:50:44 +0200

    auth handler implemented

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials"
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                const res = await fetch("http://localhost:8080/login", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: { "Content-Type": "application/json" }
+                });
+
+                const user = await res.json()
+                if (res.ok && user) {
+                    return user;
+                } else return null;
+            }
+        })
+    ]
+});
+
+export { handler as GET, handler as POST };
