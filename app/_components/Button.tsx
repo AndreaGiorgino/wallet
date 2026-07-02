846db3a2b33b6210dd3e82a39175900e35cd1101
giorgi.page <andreagiorgino1@gmail.com>
2026-07-02 19:50:12 +0200

    button updated

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button({
     onClick,
 }: ButtonProps) {
     return (
-        <button type={type} className={`invert inline-flex h-[2.5em] items-center justify-center rounded-full bg-neutral-950 px-12 py-2 font-medium text-neutral-50 transition active:scale-110 hover:scale-95 ${className}`} onClick={onClick}>
+        <button type={type} className={`invert inline-flex h-[2.5em] items-center justify-center rounded-full bg-neutral-950 px-12 py-2 font-medium text-neutral-50 transition active:scale-95 hover:scale-110 cursor-pointer ${className}`} onClick={onClick}>
             {label}
         </button>
     );
