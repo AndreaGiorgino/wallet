003f22a33d43b35f32c01c262955b33029b1c47f
giorgi.page <andreagiorgino1@gmail.com>
2026-07-02 19:52:19 +0200

    access form session managment implemented

--- app/_components/AccessForm.tsx
+++ app/_components/AccessForm.tsx
@@ -4,26 +4,35 @@ import { useState } from "react";
 import Button from "./Button";
 import LogInForm from "./LoginForm";
 import SignInForm from "./SignInForm";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Spinner from "./Spinner/Spinner";
 
 enum FormState {
     LogIn, SignIn
 };
 
 export default function AccessForm() {
+    const session = useSession();
     const [formState, setFormState] = useState<FormState>(FormState.LogIn);
 
-    function getTranslatePercentage(): number {
-        switch (formState) {
-            default:
-            case FormState.LogIn: return 0;
-            case FormState.SignIn: return -100;
-        }
+    if (session) {
+        if (session.status === "authenticated")
+            redirect("/dashboard");
+        else if (session.status === "loading")
+            return (
+                <div className="flex justify-center w-full mt-16">
+                    <div className="bg-neutral-900 rounded-lg p-6 w-[15em] flex justify-center">
+                        <Spinner></Spinner>
+                    </div>
+                </div>
+            );
     }
 
     return (
         <div className="w-full mt-16 flex flex-col gap-12 items-center">
             <div className="overflow-hidden w-[30em]">
-                <div className="flex transition" style={{ transform: `translateX(${getTranslatePercentage()}%)` }}>
+                <div className="flex transition" style={{ transform: `translateX(${formState as number * -100}%)` }}>
                     <LogInForm></LogInForm>
                     <SignInForm></SignInForm>
                 </div>
