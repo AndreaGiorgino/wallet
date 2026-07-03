dc6c44ef8fef2b8836f866c90dd749fb51cc796d
giorgi.page <andreagiorgino1@gmail.com>
2026-07-03 18:42:32 +0200

    navbar component implented

---
+++ app/dashboard/_components/Navbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { FaWallet } from "react-icons/fa"
+import { GrTransaction } from "react-icons/gr"
+import { MdPerson } from "react-icons/md"
+import NavButton from "./NavButton"
+
+const profileLogo = <MdPerson size={32} />
+const walletLogo = <FaWallet size={32} />
+const transactionsLogo = <GrTransaction size={32} />
+
+export default function Navbar() {
+    return (
+        <div className="absolute bottom-[2em] left-0 w-full flex justify-center">
+            <ul className="flex gap-2 px-2 py-1 list-none rounded-lg bg-neutral-900 w-fit">
+                <li>
+                    <NavButton label="Profile" callbackUrl="/dashboard/profile" children={profileLogo}></NavButton>
+                </li>
+                <li>
+                    <NavButton label="Wallet" callbackUrl="/dashboard/wallet" children={walletLogo}></NavButton>
+                </li>
+                <li>
+                    <NavButton label="Transactions" callbackUrl="/dashboard/transactions" children={transactionsLogo}></NavButton>
+                </li>
+            </ul>
+        </div>
+    )
+}
