3170b881c24c43b9408be543b6c4c36d0ca0bbab
giorgi.page <andreagiorgino1@gmail.com>
2026-07-05 21:31:10 +0200

    app logo added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
+import Image from "next/image";
 import AccessForm from "./_components/AccessForm";
 
 export default function Home() {
     return (
         <div className="flex flex-col flex-1 justify-center items-center font-sans bg-zinc-50 dark:bg-black">
             <main className="flex flex-col flex-1 items-center px-16 py-32 w-full max-w-3xl bg-white dark:bg-black sm:items-start">
-                <div className="flex flex-col gap-5 items-center w-full">
-                    <h1 className="text-5xl">WALLET</h1>
-                    <h6>Track your funds and transactions</h6>
+                <div className="flex justify-center w-full">
+                    <div className="flex items-center justify-center w-[30em]">
+                        <Image alt="wallet" src={"/wallet.png"} loading="eager" width={200} height={200} className="mx-[-2em]" />
+                        <div className="flex flex-col gap-5">
+                            <h1 className="text-5xl">WALLET</h1>
+                            <h6>Track your funds and transactions</h6>
+                        </div>
+                    </div>
                 </div>
                 <AccessForm></AccessForm>
             </main>
