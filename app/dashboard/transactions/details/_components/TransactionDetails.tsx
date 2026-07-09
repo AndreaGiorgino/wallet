ea4e0f266695cb78fc9ce4d69671c104df29d4f2
giorgi.page <andreagiorgino1@gmail.com>
2026-07-09 15:25:03 +0200

    fetch dropdowns data implemented

--- app/dashboard/transactions/details/_components/TransactionDetails.tsx
+++ app/dashboard/transactions/details/_components/TransactionDetails.tsx
@@ -3,7 +3,8 @@
 import Button from "@/app/_components/Button";
 import DropdownButton from "@/app/_components/DropdownButton";
 import ErrorMessage from "@/app/_components/ErrorMessage";
-import Loader from "@/app/_components/Spinner/Loader";
+import Loader from "@/app/_components/Loader/Loader";
+import Paper from "@/app/_components/Paper";
 import TextInput from "@/app/_components/TextInput";
 import { useSession } from "next-auth/react";
 import { useSearchParams } from "next/navigation";
@@ -42,26 +43,35 @@ export default function TransactionDetails() {
             return;
 
         setError("");
-        // TODO: function "fetchData" not implemented yet
+        try {
+            const res = await fetch("http://localhost:8080/config/transaction_types", {
+                method: "GET",
+            });
 
-        // TODO: fetch data
-        setTransactionTypes([
-            "Card Payment",
-            "Exchange",
-            "Topup",
-            "Transfer",
-            "Other",
-        ])
+            const data = await res.json()
+            if (!res.ok || !data)
+                setError("Cannot get transaction types data.");
+            setTransactionTypes(data["transaction_types"])
+        } catch {
+            setError("Cannot get transaction types data.");
+        }
 
-        // TODO: fetch data
-        setTransactionStates([
-            "Completed",
-            "Pending",
-        ])
+        try {
+            const res = await fetch("http://localhost:8080/config/transaction_states", {
+                method: "GET",
+            });
+
+            const data = await res.json()
+            if (!res.ok || !data)
+                setError("Cannot get transaction states data.");
+            setTransactionStates(data["transaction_states"])
+        } catch {
+            setError("Cannot get transaction states data.");
+        }
     };
 
-    const handleSave = async () => {
-        // TODO: function "handleSave" not implemented yet
+    const handleSubmit = async () => {
+        // TODO: function "handleSubmit" not implemented yet
     };
 
     useEffect(() => {
@@ -75,26 +85,15 @@ export default function TransactionDetails() {
     }, [session]);
 
     return session ? (
-        <div className="flex flex-col gap-6">
-            <div className="flex gap-6 items-end">
-                <h3 className="text-2xl">
-                    {
-                        transactionId
-                            ? "Transaction details"
-                            : "New transaction"
-                    }
-                </h3>
-                <ErrorMessage text={error} />
-            </div>
-            <hr />
-            {editing ? (
-                <div className="p-4 w-full rounded-lg sm:p-6 bg-neutral-950">
-                    <form id="data-form" onSubmit={handleSave}>
-                        <div className="grid grid-cols-1 gap-6 items-end mb-6 sm:grid-cols-6">
+        <div className="flex flex-col gap-8 px-2">
+            <Paper error={error} title={transactionId ? "Transaction Details" : "New Transaction"}>
+                {editing ? (
+                    <form id="data-form" onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-2 gap-6 items-end mb-6 sm:grid-cols-6">
                             <div className="col-span-full">
                                 <TextInput id="description" placeholder="Bill description..." label="Description" defaultValue={""} required />
                             </div>
-                            <div className="relative sm:col-span-2">
+                            <div className="relative col-span-full sm:col-span-2">
                                 <span className="absolute bottom-[0.6em] left-[.75em] font-bold">&euro;</span>
                                 <TextInput id="amount" type="number" label="Amount" placeholder="Enter amount..." defaultValue="1000" className="ps-[2.5em]" required />
                             </div>
@@ -104,54 +103,52 @@ export default function TransactionDetails() {
                             <div className="sm:col-span-2">
                                 <DropdownButton label="State" items={transactionStates} className="w-full" />
                             </div>
-                            <div className="col-span-3">
+                            <div className="sm:col-span-3">
                                 <TextInput id="started_date" type="datetime-local" label="Started Date" placeholder="Enter started date..." defaultValue={datetimeNow().toISOString().slice(0, 16)} required />
                             </div>
-                            <div className="col-span-3">
+                            <div className="sm:col-span-3">
                                 <TextInput id="completed_date" type="datetime-local" label="Completed Date" placeholder="Enter completed date..." defaultValue={datetimeNow().toISOString().slice(0, 16)} required />
                             </div>
                         </div>
                     </form>
-                </div>
-            ) : (
-                <div className="p-4 w-full rounded-lg sm:p-6 bg-neutral-950">
-                    <div className="grid grid-cols-1 gap-6 items-end mb-6 sm:grid-cols-6">
+                ) : (
+                    <div className="grid grid-cols-2 gap-6 items-end mb-6 sm:grid-cols-6">
                         <div className="flex flex-col col-span-full pb-3 h-full border-b-1">
-                            <span className="text-sm">Description</span>
-                            <span className="mt-2 text-sm text-heading">{transaction?.description}</span>
+                            <span className="mb-6 text-sm">Description</span>
+                            <span className="text-sm text-heading">{transaction?.description}</span>
                         </div>
-                        <div className="flex relative flex-col pb-3 h-full border-b-1 sm:col-span-2">
-                            <span className="text-sm">Amount</span>
+                        <div className="flex relative flex-col col-span-full pb-3 h-full border-b-1 sm:col-span-2">
+                            <span className="mb-6 text-sm">Amount</span>
                             <span className="absolute bottom-[.6em] left-0 font-bold">&euro;</span>
-                            <span className="mt-2 text-sm text-heading ps-[1.5em]">{transaction?.amount}</span>
+                            <span className="text-sm text-heading ps-[1.5em]">{transaction?.amount}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1 sm:col-span-2">
-                            <span className="text-sm">Transaction Type</span>
-                            <span className="mt-2 text-sm text-heading">{transaction?.type}</span>
+                            <span className="mb-6 text-sm">Transaction Type</span>
+                            <span className="text-sm text-heading">{transaction?.type}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1 sm:col-span-2">
-                            <span className="text-sm">State</span>
-                            <span className="mt-2 text-sm text-heading">{transaction?.state}</span>
+                            <span className="mb-6 text-sm">State</span>
+                            <span className="text-sm text-heading">{transaction?.state}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1 sm:col-span-3">
-                            <span className="text-sm">Started Date</span>
-                            <span className="mt-2 text-sm text-heading">{transaction?.started_date}</span>
+                            <span className="mb-6 text-sm">Started Date</span>
+                            <span className="text-sm text-heading">{transaction?.started_date}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1 sm:col-span-3">
-                            <span className="text-sm">Completed Date</span>
-                            <span className="mt-2 text-sm text-heading">{transaction?.completed_date}</span>
+                            <span className="mb-6 text-sm">Completed Date</span>
+                            <span className="text-sm text-heading">{transaction?.completed_date}</span>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
+            </Paper>
             {editing ? (
-                <div className="flex flex-col gap-3 justify-end items-center text-sm sm:flex-row">
+                <div className="flex flex-col gap-4 justify-end items-center text-sm sm:flex-row">
                     <Button label="Cancel" onClick={() => router.push("/dashboard/transactions")} className="w-full border-2 border-neutral-100 bg-neutral-100/25 dark:invert-0 sm:w-auto" />
-                    <Button label="Save" type="submit" form="data-form" className="w-full sm:w-auto" />
+                    <Button label="Save" type="submit" form="data-form" />
                 </div>
             ) : (
                 <div className="flex flex-col justify-end items-center text-sm sm:flex-row">
-                    <Button label="Edit" onClick={() => setEditing(true)} className="w-full sm:w-auto" />
+                    <Button label="Edit" onClick={() => setEditing(true)} />
                 </div>
             )}
         </div>
