c68d80a9c7b8633f4e2d2adaff76b622b45b0780
giorgi.page <andreagiorgino1@gmail.com>
2026-07-20 20:16:33 +0200

    code refactoring

--- app/_components/SignInForm.tsx
+++ app/_components/SignInForm.tsx
@@ -11,25 +11,21 @@ export default function SignInForm() {
     const router = useRouter();
     const [error, setError] = useState<string>("");
 
-    const handleSubmit = async (e: SubmitEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const handleSubmit = async (formData: FormData) => {
         setError("");
 
-        const form = e.target;
-        const data = new FormData(form);
-
-        if (data.get("password") !== data.get("confirm_password")) {
+        if (formData.get("password") !== formData.get("confirm_password")) {
             setError("Passwords does not match.");
-            return null;
+            return;
         }
 
         const res = await fetch("http://localhost:8080/signin", {
             method: 'POST',
             body: JSON.stringify({
-                first_name: data.get("first_name"),
-                last_name: data.get("last_name"),
-                email: data.get("email"),
-                password: data.get("password"),
+                first_name: formData.get("first_name"),
+                last_name: formData.get("last_name"),
+                email: formData.get("email"),
+                password: formData.get("password"),
             }),
             headers: { "Content-Type": "application/json" }
         });
@@ -41,8 +37,8 @@ export default function SignInForm() {
         }
 
         const signInRes = await signIn("credentials", {
-            email: data.get("email"),
-            password: data.get("password"),
+            email: formData.get("email"),
+            password: formData.get("password"),
             redirect: false,
         });
 
@@ -56,30 +52,30 @@ export default function SignInForm() {
     return (
         <div className="flex flex-col gap-8 w-full max-w-[30em] px-2">
             <Paper error={error} title="Sign In">
-                <form onSubmit={handleSubmit}>
+                <form id="data-from" action={handleSubmit}>
                     <div className="grid grid-cols-1 gap-6 mb-6 sm:grid-cols-2">
                         <div>
-                            <TextInput id="first_name" placeholder="Jhon" label="First name" required></TextInput>
+                            <TextInput id="first_name" placeholder="Jhon" label="First name" required />
                         </div>
                         <div>
-                            <TextInput id="last_name" placeholder="Doe" label="Last name" required></TextInput>
+                            <TextInput id="last_name" placeholder="Doe" label="Last name" required />
                         </div>
 
                         <div className="sm:col-span-2">
-                            <TextInput id="email" type="email" placeholder="john.doe@company.com" label="Email address" required></TextInput>
+                            <TextInput id="email" type="email" placeholder="john.doe@company.com" label="Email address" required />
                         </div>
 
                         <div className="sm:col-span-2">
-                            <TextInput id="password" type="password" placeholder="**********" label="Password" required></TextInput>
+                            <TextInput id="password" type="password" placeholder="**********" label="Password" required />
                         </div>
                         <div className="sm:col-span-2">
-                            <TextInput id="confirm_password" type="password" placeholder="**********" label="Confirm password" required></TextInput>
+                            <TextInput id="confirm_password" type="password" placeholder="**********" label="Confirm password" required />
                         </div>
                     </div>
                 </form>
             </Paper>
             <div className="flex flex-col justify-end items-center text-sm sm:flex-row">
-                <Button type="submit" label="Submit"></Button>
+                <Button type="submit" label="Submit" form="data-form" />
             </div>
         </div>
     );
