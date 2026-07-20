b7b28d8fdde6b306d6e77d802f3fe230d7c0c916
giorgi.page <andreagiorgino1@gmail.com>
2026-07-20 20:17:33 +0200

    transactions list rendering implemented

--- app/dashboard/transactions/_components/TransactionsList.tsx
+++ app/dashboard/transactions/_components/TransactionsList.tsx
@@ -3,6 +3,7 @@
 import Button from "@/app/_components/Button";
 import ErrorMessage from "@/app/_components/ErrorMessage";
 import Loader from "@/app/_components/Loader/Loader";
+import Paper from "@/app/_components/Paper";
 import TextInput from "@/app/_components/TextInput";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -22,8 +23,14 @@ interface Transaction {
 export default function TransactionsList() {
     const router = useRouter();
     const { data: session, status } = useSession();
-    const [transactions, setTransactions] = useState<Transaction[] | null>();
+    const [transactions, setTransactions] = useState<{ date: string, items: Transaction[] }[] | undefined>();
     const [error, setError] = useState<string>("");
+
+    const monthNames = [
+        "Jan", "Feb", "Mar", "Apr",
+        "May", "Jun", "Jul", "Aug",
+        "Sep", "Oct", "Nov", "Dec"
+    ];
 
     const fetchData = async () => {
         if (status === "loading")
@@ -45,16 +52,26 @@ export default function TransactionsList() {
                 return;
             }
 
-            setTransactions(data.transactions);
-        } catch {
+            const groups = Array.from(
+                Map.groupBy(data.transactions, ({ started_date }: Transaction) => {
+                    const date = new Date(started_date);
+                    return `${date.getDate()}, ${monthNames[date.getMonth()]}`;
+                })).map(([key, value]) => { return { date: key, items: value } });
+
+            setTransactions(groups);
+        } catch (ex) {
             setError("Something went wrong.");
-            return;
         }
     }
 
     useEffect(() => {
         fetchData();
     }, [session]);
+
+    const getTimeString = (dateString: string) => {
+        const date = new Date(dateString);
+        return `${date.getHours()}:${date.getMinutes()}`;
+    };
 
     return session ? (
         <div className="flex flex-col">
@@ -66,20 +83,46 @@ export default function TransactionsList() {
             </div>
             <ErrorMessage text={error} />
             {!error && (
-                <div className="flex flex-1 justify-center">
-                    {transactions?.length ? (
-                        <ul className="list-none">
-                            {/* TODO: render transactions list */}
-                        </ul>
+                transactions ? (
+                    transactions.length !== 0 ? (
+                        transactions.map(({ date, items }) => {
+                            return (
+                                <div key={date} className="flex flex-col flex-1 my-6">
+                                    <span className="font-bold">{date}</span>
+                                    <ul>
+                                        {items.map(transaction => {
+                                            return (
+                                                <li key={transaction.id} className="mt-3 bg-zinc-50 dark:bg-black">
+                                                    <a href={`/dashboard/transactions/details/${transaction.id}`} className="outline-0 focus:[&>*]:bg-neutral-800 hover:[&>*]:bg-neutral-800">
+                                                        <div className="flex items-start p-2 rounded-lg bg-neutral-950">
+                                                            <div className="flex flex-col flex-1">
+                                                                <span className="font-medium">{transaction.description}</span>
+                                                                <span className="text-sm text-gray-500">{getTimeString(transaction.started_date)}</span>
+                                                            </div>
+                                                            <div className={`flex gap-1 text-sm rounded-lg px-3 py-1 ${transaction.amount > 0 ? "bg-green-900/40" : "bg-red-900/40"}`}>
+                                                                <span>&euro;</span>
+                                                                <span>{Math.abs(transaction.amount)}</span>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </li>
+                                            )
+                                        })}
+                                    </ul>
+                                </div>
+                            )
+                        })
                     ) : (
-                        <div className="flex gap-3 items-center px-2 py-2 mt-6 w-full max-w-md text-sm font-bold rounded-lg bg-yellow-700/40 text-medium">
+                        <div className="inline-flex flex-1 gap-3 items-center px-2 py-2 text-sm font-medium text-white rounded-lg bg-red-900/40 text-medium bg-yellow-700/40">
                             <CgDanger size={20} />
                             No transactions found
                         </div>
-                    )}
-                </div>
+                    )
+                ) : (
+                    <Loader />
+                )
             )}
-        </div>
+        </div >
     ) : (
         <Loader />
     )
