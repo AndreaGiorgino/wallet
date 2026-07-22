6d9eeb5924442c49001c7ebf8d723ee2173fc452
giorgi.page <andreagiorgino1@gmail.com>
2026-07-22 22:21:42 +0200

    style updated

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -18,7 +18,7 @@ export default function Button({
     disabled = false,
 }: Readonly<ButtonProps>) {
     return (
-        <button type={type} className={`inline-flex h-[2.5em] items-center justify-center rounded-lg bg-zinc-950 dark:bg-white dark:text-black px-12 py-2 font-medium text-current transition ring-3 border-none active:scale-95 hover:sm:scale-110 hover:scale-103 w-full sm:w-auto focus:sm:scale-110 focus:scale-103 cursor-pointer ${disabled && "!bg-gray-500"} ${className}`} onClick={(e) => {
+        <button type={type} className={`inline-flex h-[2.5em] items-center justify-center rounded-lg bg-zinc-950 dark:bg-white dark:text-black px-12 py-2 font-medium text-current transition ring-3 border-none outline-none active:scale-95 hover:sm:scale-110 hover:scale-103 w-full sm:w-auto focus:sm:scale-110 focus:scale-103 cursor-pointer ${disabled && "!bg-gray-500"} ${className}`} onClick={(e) => {
             e.currentTarget.blur()
             onClick && onClick()
         }} form={form}>
