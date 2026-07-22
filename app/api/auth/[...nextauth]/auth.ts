2dfc8c02f6dafbbb200bdcc66273e65f514289b3
giorgi.page <andreagiorgino1@gmail.com>
2026-07-22 15:41:31 +0200

    auth refactoring

---
+++ app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,62 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import SessionUser from "./SessionUser";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {},
+                password: {},
+            },
+            async authorize(credentials) {
+                try {
+                    const res = await fetch("http://localhost:8080/login", {
+                        method: 'POST',
+                        body: JSON.stringify(credentials),
+                        headers: { "Content-Type": "application/json" }
+                    });
+
+                    const data = await res.json() as SessionUser;
+                    if (res.ok && data) {
+                        const base64Url = data.accessToken.split(".")[1];
+                        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+                        const payload = JSON.parse(atob(base64));
+
+                        return {
+                            id: data.id,
+                            accessToken: data.accessToken,
+                            tokenExp: payload.exp,
+                            email: data.email,
+                            name: `${data.first_name} ${data.last_name}`,
+                        };
+                    }
+                    return null;
+                } catch (ex) {
+                    return null;
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token = {
+                    accessToken: user.accessToken,
+                    tokenExp: user.tokenExp,
+                    email: user.email,
+                    name: user.name,
+
+                }
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
+};
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,6 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
-import UserData from "./UserData";
+import { authOptions } from "./auth";
 
-const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            credentials: {
-                email: {},
-                password: {},
-            },
-            async authorize(credentials) {
-                try {
-                    const res = await fetch("http://localhost:8080/login", {
-                        method: 'POST',
-                        body: JSON.stringify(credentials),
-                        headers: { "Content-Type": "application/json" }
-                    });
-
-                    const data = await res.json() as UserData;
-                    if (res.ok && data) {
-                        const base64Url = data.accessToken.split(".")[1];
-                        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-                        const payload = JSON.parse(atob(base64));
-
-                        return {
-                            id: data.id,
-                            accessToken: data.accessToken,
-                            tokenExp: payload.exp,
-                            email: data.email,
-                            name: `${data.first_name} ${data.last_name}`,
-                        };
-                    }
-                    return null;
-                } catch (ex) {
-                    return null;
-                }
-            }
-        })
-    ],
-    session: {
-        strategy: "jwt",
-    },
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token = {
-                    accessToken: user.accessToken,
-                    tokenExp: user.tokenExp,
-                    email: user.email,
-                    name: user.name,
-
-                }
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if (token)
-                session.accessToken = token.accessToken;
-            return session;
-        },
-    },
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
