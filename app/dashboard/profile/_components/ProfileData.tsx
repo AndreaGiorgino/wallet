aece9584e695b952b6b6faa3f0f218bc2b55df44
giorgi.page <andreagiorgino1@gmail.com>
2026-07-22 22:21:36 +0200

    keep form state implemented

--- app/dashboard/profile/_components/ProfileData.tsx
+++ app/dashboard/profile/_components/ProfileData.tsx
@@ -20,10 +20,10 @@ export default function ProfileData({ user }: { user?: User }) {
     if (!user)
         return <ErrorMessage text="Failed to load profile data." />
 
-    const router = useRouter()
     const { data: session } = useSession()
     const [pending, startTransition] = useTransition()
 
+    const formState = user;
     const [editing, setEditing] = useState<boolean>(false)
     const [error, setError] = useState<string>("")
 
@@ -31,11 +31,16 @@ export default function ProfileData({ user }: { user?: User }) {
         startTransition(async () => {
             const res = await profileUpdate(formData)
 
-            if (res?.success)
+            if (res?.success) {
                 setEditing(false)
-            setError(res?.error ?? "")
+                await refreshProfile()
+            } else {
+                formState.first_name = formData.get("first_name")!.toString()
+                formState.last_name = formData.get("last_name")!.toString()
+                formState.email = formData.get("email")!.toString()
+            }
 
-            await refreshProfile()
+            setError(res?.error ?? "")
         })
     }
 
@@ -46,13 +51,13 @@ export default function ProfileData({ user }: { user?: User }) {
                     <form id="data-form" action={handleSubmit}>
                         <div className="grid grid-cols-1 gap-6 mb-6 sm:grid-cols-2">
                             <div>
-                                <TextInput name="first_name" placeholder="Jhon" label="First name" defaultValue={user?.first_name} required></TextInput>
+                                <TextInput name="first_name" placeholder="Jhon" label="First name" defaultValue={user.first_name} required></TextInput>
                             </div>
                             <div>
-                                <TextInput name="last_name" placeholder="Doe" label="Last name" defaultValue={user?.last_name} required></TextInput>
+                                <TextInput name="last_name" placeholder="Doe" label="Last name" defaultValue={user.last_name} required></TextInput>
                             </div>
                             <div className="sm:col-span-2">
-                                <TextInput name="email" type="email" placeholder="john.doe@company.com" label="Email address" defaultValue={user?.email} required></TextInput>
+                                <TextInput name="email" type="email" placeholder="john.doe@company.com" label="Email address" defaultValue={user.email} required></TextInput>
                             </div>
                         </div>
                     </form>
@@ -60,22 +65,22 @@ export default function ProfileData({ user }: { user?: User }) {
                     <div className="grid grid-cols-1 gap-6 items-end mb-6 sm:grid-cols-2">
                         <div className="flex flex-col pb-3 h-full border-b-1">
                             <span className="text-sm">First name</span>
-                            <span className="mt-2 text-sm text-heading">{user?.first_name}</span>
+                            <span className="mt-2 text-sm text-heading">{user.first_name}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1">
                             <span className="text-sm">Last name</span>
-                            <span className="mt-2 text-sm text-heading">{user?.last_name}</span>
+                            <span className="mt-2 text-sm text-heading">{user.last_name}</span>
                         </div>
                         <div className="flex flex-col pb-3 h-full border-b-1 sm:col-span-2">
                             <span className="text-sm">Email address</span>
-                            <span className="mt-2 text-sm text-heading">{user?.email}</span>
+                            <span className="mt-2 text-sm text-heading">{user.email}</span>
                         </div>
                     </div>
                 )}
             </Paper>
             {editing ? (
                 <div className="flex flex-col gap-4 justify-end items-center text-sm sm:flex-row">
-                    <Button label="Cancel" onClick={() => setEditing(false)} className="w-full border-2 invert border-neutral-100 sm:w-auto" />
+                    <Button label="Cancel" onClick={() => setEditing(false)} className="w-full invert sm:w-auto" />
                     <Button label="Save" type="submit" form="data-form" />
                 </div>
             ) : (
