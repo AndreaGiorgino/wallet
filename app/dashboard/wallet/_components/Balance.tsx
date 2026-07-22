fbb9051ed567999bfa33bd8b70e97b74a8bbbce3
giorgi.page <andreagiorgino1@gmail.com>
2026-07-22 15:42:24 +0200

    wallet ssc conversion

--- app/dashboard/wallet/_components/Balance.tsx
+++ app/dashboard/wallet/_components/Balance.tsx
@@ -3,52 +3,19 @@
 import ErrorMessage from "@/app/_components/ErrorMessage";
 import Loader from "@/app/_components/Loader/Loader";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react"
-import { CgDanger } from "react-icons/cg";
 
-export default function Balance() {
-    const { data: session, status } = useSession();
-    const [balance, setBalance] = useState<number | null>(null);
-    const [error, setError] = useState<string>("");
+export default function Balance({ amount }: { amount?: number }) {
+    if (amount === undefined)
+        return <ErrorMessage text="Failed to load balance amount." />
 
-    const fetchData = async () => {
-        if (status === "loading")
-            return;
-
-        setError("");
-        const res = await fetch("http://localhost:8080/wallet/balance", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${session?.accessToken}`,
-            },
-        });
-
-        const data = await res.json();
-        if (!res?.ok || !data) {
-            setError("Cannot get balance data.")
-            return;
-        }
-
-        setBalance(data.balance);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [session]);
+    const { data: session } = useSession();
 
     return session ? (
-        <div className="flex flex-col gap-8 px-2">
-            <div className="flex gap-6 items-end">
-                <ErrorMessage text={error} />
-            </div>
-            <div className="flex justify-center">
-                <div className="flex gap-2 px-24 py-12 text-3xl rounded-full shadow-lg text-medium dark:shadow-white">
-                    <span>{balance}</span>
-                    <span>&euro;</span>
-                </div>
+        <div className="flex justify-center">
+            <div className="flex gap-2 px-24 py-12 text-3xl rounded-full shadow-lg text-medium dark:shadow-white">
+                <span>{amount}</span>
+                <span>&euro;</span>
             </div>
         </div>
-    ) : (
-        <Loader />
-    )
+    ) : <Loader />
 }
--- app/dashboard/wallet/page.tsx
+++ app/dashboard/wallet/page.tsx
@@ -1,9 +1,30 @@
+import { getServerSession } from "next-auth";
 import Balance from "./_components/Balance";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
 
-export default function Wallet() {
+export default async function Wallet() {
+    const session = await getServerSession(authOptions);
+
+    const amount = await (async () => {
+        try {
+            const res = await fetch("http://localhost:8080/wallet/balance", {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${session?.accessToken}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const data = await res.json();
+            if (!res.ok || !data)
+                throw {};
+            return data.balance;
+        } catch { }
+    })();
+
     return (
         <div className="flex flex-col flex-1 gap-6 w-full">
-            <Balance />
+            <Balance amount={amount} />
         </div>
     );
 }
