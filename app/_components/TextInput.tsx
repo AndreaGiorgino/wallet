2897a906c99bb975d4816c8fd5c498fe24a6c66e
giorgi.page <andreagiorgino1@gmail.com>
2026-07-23 18:48:14 +0200

    onchange handler added

--- app/_components/TextInput.tsx
+++ app/_components/TextInput.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import { useState } from "react"
+import { ChangeEventHandler, useState } from "react"
 
 interface TextInputProps {
     name: string,
     type?: string,
     defaultValue?: string,
+    onChange?: ChangeEventHandler<HTMLInputElement, HTMLInputElement>,
     placeholder?: string,
     required?: boolean,
     label?: string,
@@ -20,6 +21,7 @@ export default function TextInput({
     name,
     type = "text",
     defaultValue = "",
+    onChange = () => { },
     placeholder,
     required = false,
     label,
@@ -34,7 +36,10 @@ export default function TextInput({
     return (
         <label className={`block w-full ${className}`}>
             {label && <span className="text-sm">{label}</span>}
-            <input type={type} name={name} className={`bg-zinc-50 dark:bg-black bg-zinc-50 border-2 border-current/25 text-heading text-sm text-current rounded-xl outline-none transition focus:ring-blue-400 block w-full px-3 py-2.5  placeholder:text-body ring-2 ${label && "mt-2"}`} placeholder={placeholder} required={required} min={min} max={max} step={step} disabled={disabled} value={value} onChange={e => setValue(e.target.value)} />
+            <input type={type} name={name} className={`bg-zinc-50 dark:bg-black bg-zinc-50 border-2 border-current/25 text-heading text-sm text-current rounded-xl outline-none transition focus:ring-blue-400 block w-full px-3 py-2.5  placeholder:text-body ring-2 ${label && "mt-2"}`} placeholder={placeholder} required={required} min={min} max={max} step={step} disabled={disabled} value={value} onChange={e => {
+                setValue(e.target.value)
+                onChange(e)
+            }} />
         </label>
     )
 }
