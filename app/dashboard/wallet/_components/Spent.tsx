d6b5ad1c84e2f19cb701be619d173b76b299fec0
giorgi.page <andreagiorgino1@gmail.com>
2026-07-24 16:19:45 +0200

    current month spent component implemented

---
+++ app/dashboard/wallet/_components/Spent.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import ErrorMessage from "@/app/_components/ErrorMessage"
+import Paper from "@/app/_components/Paper"
+import { useEffect, useState } from "react"
+import { Transaction } from "../../transactions/details/[[...slug]]/_components/TransactionDetails"
+import { useSession } from "next-auth/react"
+import SpentChart from "./SpentChart"
+
+export default function Spent({ transactions }: { transactions?: Transaction[] }) {
+    if (!transactions)
+        return <ErrorMessage text="Failed to load current month transactions." />
+
+    const { status } = useSession()
+
+    const [state, setState] = useState<Transaction[]>(transactions!)
+
+    const amount = transactions!.filter(transaction => transaction.amount < 0)
+        .map(transaction => -transaction.amount)
+        .reduce((prev, next) => prev + next, 0)
+
+    useEffect(() => {
+        if (transactions)
+            setState(transactions);
+    }, [transactions])
+
+    if (status === "loading")
+        return
+
+    return (
+        <Paper title="Spent this month">
+            <span className="font-medium">&euro; {amount}</span>
+            <SpentChart transactions={state} />
+        </Paper>
+    )
+}
